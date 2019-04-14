4e2a33d052a89a2fe22db10bed3cb368802a3e32
sheliakst <stamcj@hotmail.com>
2019-04-14 19:46:43 +0300

    initial commit

--- js/loaders.js
+++ js/loaders.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
-
-    $("nav li").click(function(){
-        $(".wrapper").addClass("hide");
-
-    });
-  setTimeout(function() {
-    $(".preload").removeClass("addPreload");
-    $(".wrapper").removeClass("shadow");
-    $(".wrapper").addClass("show");
-  }, 1000);
-  $(".wrapper").remove("show");
+/* $(document).ready(function() {
+  $(" li").click(function() {
+    event.preventDefault();
+    var newLink = $("a", this).attr("href");
+console.log(22222);
+    $("body").fadeOut(5000, navigation(newLink));
+  });
 });
+
+function navigation(link) {
+  window.location = link;
+}
+     */
